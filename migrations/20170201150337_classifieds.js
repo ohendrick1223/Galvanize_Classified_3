ca4fcaf31b6b5954dccfa2c773c645fc386d9f5b
Olivia <ohendrick1223@gmail.com>
2017-02-03 10:45:22 -0700

    push changes to heroku

--- migrations/20170201150337_classifieds.js
+++ migrations/20170201150337_classifieds.js
@@ -2,10 +2,10 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('classifieds', function(table) {
     table.increments();
-    table.varchar('title', 'char(255)').notNullable();
-    table.varchar('description', 'char(255)').notNullable();
-    table.decimal('price').notNullable();
-    table.varchar('item_image', 'char(255)').notNullable();
+    table.string('title').notNullable();
+    table.string('description', 2000).notNullable();
+    table.string('price').notNullable();
+    table.string('item_image', 2000).notNullable();
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
   });
